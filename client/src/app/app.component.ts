4b17f2216fb232fe2896307b09d69c65dc43cc42
sandras15 <sandras15@ru.is>
2017-02-17 19:36:14 +0000

    Lecture 5a done

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import * as io from "socket.io-client";
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,25 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app works!';
+  socket : any;
+  userName : string;
+  loginFailed : boolean = false; 
+
+  constructor(){
+  	this.socket = io("http://localhost:8080");
+  	this.socket.on("connect", function(){
+    	console.log("connect")
+	});
+  }
+
+  onLogin(){
+  	this.socket.emit("adduser", this.userName, succeeded => {
+    	if (!succeeded) { 
+    		this.loginFailed = true;
+    	} 
+    	else {
+    		console.log("Login succeeded!")
+    	}
+	});
+  }
 }
