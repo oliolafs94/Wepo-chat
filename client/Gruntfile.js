67d708da3fc50517015dad1d0cfe3beed7b6cbb4
orvarb <orvargud@gmail.com>
2017-02-19 15:43:11 +0000

    Gruntfile can be executed using cmd grunt in client

--- client/Gruntfile.js
+++ client/Gruntfile.js
@@ -1,20 +1,18 @@
-module.export = function(grunt) {
 
-	// Project configuration
-	var taskConfig = {
-		pgk: grunt.file.readJSON('package.json'),
-		tslint:  {
-			src: ['src/**/*.ts'],
-			gruntfile: ['Gruntfile.js'],
-			options: {
-				configuration: "tslint.json",
-				force: true,
-				fix: false
-			},
-		}
-	};
-	grunt.initConfig(taskConfig);
-	grunt.registerTask('default', ['tslint']); 
-	//grunt.registerTask('default', ['tslint']);
-	grunt.loadNpmTask('grunt-tslint');
+module.exports = function(grunt) {
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
+    tslint: {
+      src: ['src/**/*.ts'],
+      gruntfile: ['Gruntfile.js'],
+      options: {
+        configuration: "tslint.json",
+        force: true,
+        fix: false
+      }
+    }
+  });
+
+  grunt.loadNpmTasks('grunt-tslint');
+  grunt.registerTask('default', ['tslint']);
 };
